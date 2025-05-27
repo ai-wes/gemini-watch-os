8ede39a6bd2ed2b8b41623f76050f16eacd4c451
AI-Wes <132653112+ai-wes@users.noreply.github.com>
2025-05-26 17:42:30 -0700

    WORKING with DUMMY IMPLEMENTATIONS REMOVED

---
+++ NotizeniOSController/NotizeniOSControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotizeniOSControllerApp.swift
+//  NotizeniOSController
+//
+//  Created by Wesley Lagarde on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NotizeniOSControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
